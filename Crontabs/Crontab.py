3b6f6e19a01a6c908bd0ea4be45e826e435dc029
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2023-09-17 13:21:11 +0200

    Fixed Crontab

--- Crontabs/Crontab.py
+++ Crontabs/Crontab.py
@@ -3,6 +3,11 @@ import subprocess
 import datetime
 import os
 
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+env = os.environ.copy()
+env['PYTHONPATH'] = path
+env['PATH'] = os.pathsep.join([path, os.environ.get('PATH', '')])
+
 directory = os.path.dirname(os.path.abspath(__file__))
 counterFile = os.path.join(directory, "counter.txt")
 crontabsFile = os.path.join(directory, "Crontabs.json")
@@ -22,20 +27,23 @@ scripts = {}
 with open(crontabsFile, "r") as file:
     data = json.load(file)
 
+
+#data = scripts
 for script, date in data.items():
 
     if datetime.datetime.strptime(date, "%Y-%m-%d").date() != currentDate:
         print(script)
         scriptFile = os.path.join(directory, script)
 
-        process = subprocess.Popen(['python3', scriptFile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        data[script] = currentDate.strftime("%Y-%m-%d")
+        process = subprocess.Popen(['python3', scriptFile], stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
 
         return_code = process.wait()
         output, error = process.communicate()
 
         print("Return Code:", return_code)
         print("Error Message:", error.decode())
+        if error.decode() == "": # We have executed today if it works
+            data[script] = currentDate.strftime("%Y-%m-%d")
         print(output.decode())
 
 
@@ -46,3 +54,14 @@ counter += 1
 
 with open(counterFile, "w") as file:
     file.write(str(counter))
+
+
+
+
+
+
+
+
+
+
+

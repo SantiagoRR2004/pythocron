efad15d3cb3c62d387e7a3796dea85d7c7845a18
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2024-07-27 21:05:16 +0200

    Add to crontabs and not replace

--- Crontabs/setupCrontab.py
+++ Crontabs/setupCrontab.py
@@ -1,31 +1,25 @@
 import os
-import subprocess
+from Modules import Utils
 
+if __name__ == "__main__":
 
-directory = os.path.dirname(os.path.abspath(__file__))
-every5Min = "*/5 * * * *"
-everyMin = "*/1 * * * *"
-dailyPrograms = os.path.join(directory, "Crontab.py")
+    directory = os.path.dirname(os.path.abspath(__file__))
+    every5Min = "*/5 * * * *"
+    everyMin = "*/1 * * * *"
+    dailyPrograms = os.path.join(directory, "Crontab.py")
 
-# We check that the file exists and give an error message if it does not
-if not os.path.exists(dailyPrograms):
-    raise NameError(f"File {dailyPrograms} not found")
+    # We check that the file exists and give an error message if it does not
+    if not os.path.exists(dailyPrograms):
+        raise NameError(f"File {dailyPrograms} not found")
 
-backgroundChanger = os.path.join(directory, "BackgroundChanger.py")
+    backgroundChanger = os.path.join(directory, "BackgroundChanger.py")
 
-if not os.path.exists(backgroundChanger):
-    raise NameError(f"File {backgroundChanger} not found")
+    if not os.path.exists(backgroundChanger):
+        raise NameError(f"File {backgroundChanger} not found")
 
-output = os.path.join(directory, "output.log" )
+    output = os.path.join(directory, "output.log" )
 
-
-cronTask = f"""{every5Min} python3 {dailyPrograms} >> {output} 2>&1
-{everyMin} python3 {backgroundChanger} >> {output} 2>&1"""
-
-
-command = f"echo '{cronTask}' | crontab -"
-subprocess.call(command, shell=True)
-
+    Utils.addNewCronTasks([f"{every5Min} python3 {dailyPrograms} >> {output} 2>&1", f"{everyMin} python3 {backgroundChanger} >> {output} 2>&1"])
 
 """
 */5 * * * * python3 /absolutePath/Crontabs/Crontab.py >> /absolutePath/Crontabs/output.log 2>&1
--- Modules/Utils.py
+++ Modules/Utils.py
@@ -13,3 +13,22 @@ def runTerminal(command):
             print(completed_process.stderr)
     except Exception as e:
         print("An error occurred:", e)
+
+def addNewCronTasks(newTasks:list) -> None:
+    """
+    This function will add new tasks to the crontab
+    without deleting the old ones.
+
+    Args:
+        newTasks (list): A list of strings with the new tasks.
+
+    Returns:
+        None
+    """
+    result = subprocess.run(['crontab', '-l'], capture_output=True, text=True, check=True)
+
+    if not result.stderr:
+        oldTasks = result.stdout.split("\n")
+        for task in newTasks:
+            if task not in oldTasks:
+                runTerminal(f"(crontab -l; echo '{task}') | crontab -")

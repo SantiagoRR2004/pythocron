79bdc9575ed254b4e6d5ee4a4f5d7b97abf3f855
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2024-09-06 16:28:35 +0200

    Add environment variables to crontab setup script

--- Crontabs/setupCrontab.py
+++ Crontabs/setupCrontab.py
@@ -8,6 +8,19 @@ if __name__ == "__main__":
     everyMin = "*/1 * * * *"
     dailyPrograms = os.path.join(directory, "Crontab.py")
 
+    otherInstructionsList = [
+        "export DISPLAY=:1",
+        "export XAUTHORITY=/run/user/1001/gdm/Xauthority",
+        "export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin",
+    ]
+
+    otherInstructions = ""
+
+    for instruction in otherInstructionsList:
+        otherInstructions += f"{instruction} && "
+    else:
+        otherInstructions = otherInstructions[:-1]  # Remove the last space
+
     # We check that the file exists and give an error message if it does not
     if not os.path.exists(dailyPrograms):
         raise NameError(f"File {dailyPrograms} not found")
@@ -17,11 +30,16 @@ if __name__ == "__main__":
     if not os.path.exists(backgroundChanger):
         raise NameError(f"File {backgroundChanger} not found")
 
-    output = os.path.join(directory, "output.log" )
+    output = os.path.join(directory, "output.log")
 
-    Utils.addNewCronTasks([f"{every5Min} python3 {dailyPrograms} >> {output} 2>&1", f"{everyMin} python3 {backgroundChanger} >> {output} 2>&1"])
+    Utils.addNewCronTasks(
+        [
+            f"{every5Min} {otherInstructions} python3 {dailyPrograms} >> {output} 2>&1",
+            f"{everyMin} {otherInstructions} python3 {backgroundChanger} >> {output} 2>&1",
+        ]
+    )
 
 """
-*/5 * * * * python3 /absolutePath/Crontabs/Crontab.py >> /absolutePath/Crontabs/output.log 2>&1
-*/1 * * * * python3 /absolutePath/Crontabs/BackgroundChanger.py >> /absolutePath/Crontabs/output.log 2>&1
+*/5 * * * * export DISPLAY=:1 && export XAUTHORITY=/run/user/1001/gdm/Xauthority && export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin && python3 /absolutePath/Crontabs/Crontab.py >> /absolutePath/Crontabs/output.log 2>&1
+*/1 * * * * export DISPLAY=:1 && export XAUTHORITY=/run/user/1001/gdm/Xauthority && export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin && python3 /absolutePath/Crontabs/BackgroundChanger.py >> /absolutePath/Crontabs/output.log 2>&1
 """

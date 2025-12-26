84fecda5c760ab2cbc7496f179f95a2947c9948d
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-12-26 22:22:18 +0100

    Receive the virtual environment

--- Crontabs/runCron.sh
+++ Crontabs/runCron.sh
@@ -3,17 +3,25 @@
 # Get the directory of the current script
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+
+# Check if the virtual environment was provided
+if [ -z "$1" ]; then
+    echo "No virtual environment given" >>"${script_dir}/output.log"
+    exit 1
+fi
+
 # Activate the virtual environment
-source "${script_dir}/../.venv/bin/activate"
+source "${1}"
+
 
 # Check if a script name was provided
-if [ -z "$1" ]; then
+if [ -z "$2" ]; then
     echo "No python script given" >>"${script_dir}/output.log"
     exit 1
 fi
 
-# Get the Python script name from the first argument
-pythonScript="$1"
+# Get the Python script name from the second argument
+pythonScript="$2"
 
 # Run the Python script and log output
 python3 $pythonScript >>"${script_dir}/output.log" 2>&1

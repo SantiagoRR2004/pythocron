e16fc685c05bf6175eda3e11bcfb1da5c57fa919
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-12-31 20:32:39 +0100

    Move functions

--- Crontabs/CrontabManager.py
+++ Crontabs/CrontabManager.py
@@ -97,6 +97,60 @@ class CrontabManager:
         with open(self.counterFile, "w") as file:
             file.write(str(counter + 1))
 
+    @staticmethod
+    def runTerminal(command: str) -> None:
+        """
+        Run a terminal command and print the output or error.
+
+        TODO: Use this function from modules instead of duplicating code.
+
+        Args:
+            - command (str): The command to run.
+
+        Returns:
+            - None
+        """
+        try:
+            completed_process = subprocess.run(
+                command,
+                shell=True,
+                text=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+            if completed_process.returncode == 0:
+                print("Command executed successfully!")
+                print("Output:")
+                print(completed_process.stdout)
+            else:
+                print("Command failed!")
+                print("Error:")
+                print(completed_process.stderr)
+        except Exception as e:
+            print("An error occurred:", e)
+
+    @staticmethod
+    def addNewCronTasks(newTasks: list) -> None:
+        """
+        This function will add new tasks to the crontab
+        without deleting the old ones.
+
+        Args:
+            newTasks (list): A list of strings with the new tasks.
+
+        Returns:
+            None
+        """
+        result = subprocess.run(
+            ["crontab", "-l"], capture_output=True, text=True, check=True
+        )
+
+        if not result.stderr:
+            oldTasks = result.stdout.split("\n")
+            for task in newTasks:
+                if task not in oldTasks:
+                    CrontabManager.runTerminal(f"(crontab -l; echo '{task}') | crontab -")
+
 
 if __name__ == "__main__":
     CrontabManager().main()
